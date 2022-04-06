36e8901aacc59ac9bf0cca40f2f0f1e6a351481d
Brody Losh <brodylosh@gmail.com>
2022-04-06 16:32:46 -0400

    commit tree 25483ff9bc9c404cf1c6dcb5f1cc2c69bf8d2d07

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+let writeCards = array => {
+    for (let i = 0; i < array.length; i++) {
+        return array.push[`Thank you, ${array[i]}, for the wonderful surprise gift!`];
+      }
+      return array;
+}
